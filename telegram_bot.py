4983940264e6775d03992e53a624ad6562db7df2
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-02 21:12:17 +0500

    create keyboards

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,19 +1,17 @@
 import os
 from aiogram import Bot, Dispatcher, types, executor
 from dotenv import load_dotenv, find_dotenv
+import keyboards as kb
+
+load_dotenv(find_dotenv())
+bot = Bot(token=os.environ.get('TELEGRAM_TOKEN'))
+dp = Dispatcher(bot)
 
 
-def start():
-    load_dotenv(find_dotenv())
-    bot = Bot(token=os.environ.get('TELEGRAM_TOKEN'))
-    dp = Dispatcher(bot)
-    dp.register_message_handler(lambda message: send_echo(message))
-    executor.start_polling(dp, skip_updates=True)
-
-
-async def send_echo(message: types.Message):
-    await message.reply(text=message.text)
+@dp.message_handler(commands=['start'])
+async def process_hello(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Привет! Я бот для викторин!', reply_markup=kb.question_button_kb)
 
 
 if __name__ == '__main__':
-    start()
+    executor.start_polling(dp, skip_updates=True)
