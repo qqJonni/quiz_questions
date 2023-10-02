06d73b4817521fdbf22dd8e439e1f61c71e325b6
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-02 19:53:12 +0500

    add func quiz_questions

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,4 +1,16 @@
+with open('1vs1200.txt', 'r', encoding="KOI8-R") as file:
+    content = file.read()
+text = content.split('\n\n')
 
-with open("1vs1200.txt", "r", encoding="KOI8-R") as my_file:
-    file_contents = my_file.read()
-print(file_contents)
+questions = []
+answers = []
+
+for i in text:
+    i.strip()
+    if i.startswith('Вопрос') or i.startswith('\nВопрос'):
+        questions.append(i)
+    elif i.startswith('Ответ'):
+        answers.append(i)
+
+quiz_dictionary = dict(zip(questions, answers))
+print(quiz_dictionary)
