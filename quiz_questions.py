bdf20aeff7369b7173f8f48550c0ad4d514e91e3
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-11 15:15:19 +0500

    вернулся на aiogram. Добавил логику ответов

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,25 +1,25 @@
 from random import randint
 
 
-def random_question():
-    with open('1vs1200.txt', 'r', encoding="KOI8-R") as file:
-        content = file.read()
-    text = content.split('\n\n')
-
-    questions = []
-    answers = []
-
-    for i in text:
-        i.strip()
-        if i.startswith('Вопрос') or i.startswith('\nВопрос'):
-            questions.append(i)
-        elif i.startswith('Ответ'):
-            answers.append(i)
-
-    quiz_dictionary = dict(zip(questions, answers))
-    quiz_list = list(quiz_dictionary.items())
-
-    key, value = quiz_list[randint(0, len(quiz_list) - 1)]
-    lst = key, value
-    print(lst)
-    return lst
+# def random_question():
+#     with open('1vs1200.txt', 'r', encoding="KOI8-R") as file:
+#         content = file.read()
+#     text = content.split('\n\n')
+#
+#     questions = []
+#     answers = []
+#
+#     for i in text:
+#         i.strip()
+#         if i.startswith('Вопрос') or i.startswith('\nВопрос'):
+#             questions.append(i)
+#         elif i.startswith('Ответ'):
+#             answers.append(i)
+#
+#     quiz_dictionary = dict(zip(questions, answers))
+#     quiz_list = list(quiz_dictionary.items())
+#
+#     key, value = quiz_list[randint(0, len(quiz_list) - 1)]
+#     lst = key, value
+#
+#     return lst
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,31 +1,75 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import os
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
 from dotenv import load_dotenv, find_dotenv
+from random import randint
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+load_dotenv(find_dotenv())
+bot = Bot(os.environ.get('TELEGRAM_TOKEN'))
+dp = Dispatcher(bot)
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(KeyboardButton('Новый вопрос'))
+keyboard.insert(KeyboardButton('Сдаться'))
+keyboard.add(KeyboardButton('Мой счет'))
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот для викторин!")
+def random_question():
+    with open('1vs1200.txt', 'r', encoding="KOI8-R") as file:
+        content = file.read()
+    text = content.split('\n\n')
+
+    questions = []
+    answers = []
+
+    for i in text:
+        i.strip()
+        if i.startswith('Вопрос') or i.startswith('\nВопрос'):
+            questions.append(i)
+        elif i.startswith('Ответ'):
+            answers.append(i)
+
+    quiz_dictionary = dict(zip(questions, answers))
+    quiz_list = list(quiz_dictionary.items())
+
+    key, value = quiz_list[randint(0, len(quiz_list) - 1)]
+    lst = key, value
+
+    return lst
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+question, answer = random_question()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer(text='Привет! Я бот для викторин!', reply_markup=keyboard)
+    await message.delete()
+
+
+@dp.message_handler()
+async def new_question(message: types.Message):
+    if message.text == 'Новый вопрос':
+        global question, answer
+        question, answer = random_question()
+        print(question)
+        print(answer)
+        await message.answer(text=question)
+        await message.delete()
+
+
+@dp.message_handler()
+async def random_answer(message: types.Message):
+    if answer in message.text:
+        await message.reply('Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос".')
+    else:
+        await message.reply('Неправильно… Попробуешь ещё раз?')
+
+
+@dp.message_handler()
+async def handle_message(message: types.Message):
+    await message.answer("Не знаю ответа. Выберите вариант из меню.")
 
 
 if __name__ == '__main__':
-    load_dotenv(find_dotenv())
-    application = ApplicationBuilder().token(os.environ.get('TELEGRAM_TOKEN')).build()
-
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT and (~filters.COMMAND), echo)
-
-    application.add_handler(start_handler)
-    application.add_handler(echo_handler)
-
-    application.run_polling()
+    executor.start_polling(dp, skip_updates=True)
