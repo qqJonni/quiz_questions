8edc1c2e99d0ce79e02466c39fd8bf2d70d5f4e3
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-19 15:26:09 +0400

    избавился от глобальных переменных

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,45 +1,55 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from quiz_questions import Quiz
-import os
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
-bot = Bot(os.environ.get('TELEGRAM_TOKEN'))
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
 
-quiz = Quiz()
+class QuizBot:
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        self.bot = Bot(os.environ.get('TELEGRAM_TOKEN'))
+        self.storage = MemoryStorage()
+        self.dp = Dispatcher(self.bot, storage=self.storage)
+        self.quiz = Quiz()
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
-    button_new_question = types.KeyboardButton('Новый вопрос')
-    button_give_up = types.KeyboardButton('Сдаться')
-    button_my_score = types.KeyboardButton('Мой счет')
-    keyboard.add(button_new_question, button_give_up, button_my_score)
-    await message.answer("Привет! Я бот для викторин! Чтобы начать, нажмите кнопку 'Новый вопрос'.", reply_markup=keyboard)
+        self.register_handlers()
 
-@dp.message_handler(lambda message: message.text == 'Новый вопрос')
-async def send_new_question(message: types.Message):
-    question, answer = quiz.get_random_question()
-    await dp.storage.set_data(chat=message.chat.id, data={"answer": answer})
-    await bot.send_message(chat_id=message.chat.id, text=question)
+    def register_handlers(self):
+        @self.dp.message_handler(commands=['start'])
+        async def start(message: types.Message):
+            keyboard = types.ReplyKeyboardMarkup(row_width=3, resize_keyboard=True)
+            button_new_question = types.KeyboardButton('Новый вопрос')
+            button_give_up = types.KeyboardButton('Сдаться')
+            button_my_score = types.KeyboardButton('Мой счет')
+            keyboard.add(button_new_question, button_give_up, button_my_score)
+            await message.answer("Привет! Я бот для викторин! Чтобы начать, нажмите кнопку 'Новый вопрос'.", reply_markup=keyboard)
 
-@dp.message_handler(lambda message: message.text == 'Сдаться')
-async def give_up(message: types.Message):
-    data = await dp.storage.get_data(chat=message.chat.id)
-    answer = data.get("answer")
-    await message.reply(f"Правильный ответ: {answer}")
+        @self.dp.message_handler(lambda message: message.text == 'Новый вопрос')
+        async def send_new_question(message: types.Message):
+            question, answer = self.quiz.get_random_question()
+            await self.dp.storage.set_data(chat=message.chat.id, data={"answer": answer})
+            await self.bot.send_message(chat_id=message.chat.id, text=question)
 
-@dp.message_handler()
-async def check_answer(message: types.Message):
-    data = await dp.storage.get_data(chat=message.chat.id)
-    answer = data.get("answer")
-    if answer and message.text.lower() in answer.lower():
-        await message.reply("Поздравляю! Это верный ответ!")
-    else:
-        await message.reply("Это не верный ответ.")
+        @self.dp.message_handler(lambda message: message.text == 'Сдаться')
+        async def give_up(message: types.Message):
+            data = await self.dp.storage.get_data(chat=message.chat.id)
+            answer = data.get("answer")
+            await message.reply(f"Правильный ответ: {answer}")
+
+        @self.dp.message_handler()
+        async def check_answer(message: types.Message):
+            data = await self.dp.storage.get_data(chat=message.chat.id)
+            answer = data.get("answer")
+            if answer and message.text.lower() in answer.lower():
+                await message.reply("Поздравляю! Это верный ответ!")
+            else:
+                await message.reply("Это не верный ответ.")
+
+    def run(self):
+        executor.start_polling(self.dp, skip_updates=True)
+
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    bot = QuizBot()
+    bot.run()
